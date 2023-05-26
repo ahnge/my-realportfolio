40a793746fb9743af4d16b2a4200a00dcad460a4
ahnge <ahnge226@gmail.com>
2023-05-26 13:30:55 +0630

    add some projects

--- components/projects/AllProjects.tsx
+++ components/projects/AllProjects.tsx
@@ -1,7 +1,10 @@
 import {
+  HtmlLogo,
+  JsLogo,
   LaravelLogo,
   MysqlLogo,
   NextLogo,
+  ReactLogo,
   StrapiLogo,
   TailwindLogo,
   WordpressLogo,
@@ -12,7 +15,7 @@ export const projects = [
   {
     id: 1,
     title: "Careerhub",
-    shortDescription: "A job portal web application",
+    shortDescription: "A job portal web application for cs50x final project",
     link: "https://careerhub.fun",
     techs: [
       <LaravelLogo key={1} />,
@@ -37,6 +40,20 @@ export const projects = [
     shortDescription: "A simple landing page using wordpress",
     link: "https://hotellinsetpyinoolwin.com",
     techs: [<WordpressLogo key={1} />],
+  },
+  {
+    id: 4,
+    title: "Splitter",
+    shortDescription: "Very simple tip calculator app",
+    link: "https://ahnge.github.io/tip-calculator-app-main/",
+    techs: [<JsLogo key={1} />, <HtmlLogo key={2} />],
+  },
+  {
+    id: 5,
+    title: "Space Tourism SPA",
+    shortDescription: "Fem challenge to practice react",
+    link: "https://space-tourism1.netlify.app/",
+    techs: [<ReactLogo key={1} />, <TailwindLogo key={2} />],
   },
 ];
 
